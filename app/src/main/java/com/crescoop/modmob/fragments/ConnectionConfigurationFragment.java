492cdbb9492972d41e227ba91cfeff8c699dd0c5
ClóvisReschke <clovisreschke@gmail.com>
2016-06-10 16:50:35 -0300

    Master

--- app/src/main/java/com/crescoop/modmob/fragments/ConnectionConfigurationFragment.java
+++ app/src/main/java/com/crescoop/modmob/fragments/ConnectionConfigurationFragment.java
@@ -2,19 +2,28 @@ package com.crescoop.modmob.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.crescoop.modmob.MyApplication;
 import com.crescoop.modmob.R;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 public class ConnectionConfigurationFragment extends Fragment {
 
     private Button saveConfButton;
+    private EditText hostEditText;
+    private EditText portEditText;
+    private EditText slaveIdEditText;
 
     public static ConnectionConfigurationFragment newInstance() {
         return new ConnectionConfigurationFragment();
@@ -25,11 +34,31 @@ public class ConnectionConfigurationFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
 
+        final EditText hostEditText = (EditText) getView().findViewById(R.id.frag_conf_edit_text_host);
+        final EditText portEditText = (EditText) getView().findViewById(R.id.frag_conf_edit_text_port);
+        final EditText slaveIdEditText = (EditText) getView().findViewById(R.id.frag_conf_edit_text_slave_id);
+
         saveConfButton = (Button) getView().findViewById(R.id.save_conf_button);
         saveConfButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MyApplication singleton = MyApplication.getInstance();
+
+                InetAddress addr = null; //the slave's address
+                try {
+                    addr = InetAddress.getByName(hostEditText.getText().toString());
+                    singleton.setAddr(addr);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+                singleton.setPort(Integer.valueOf(portEditText.getText().toString()));
+
+                singleton.setSlave_id(Integer.valueOf(slaveIdEditText.getText().toString()));
+
+
+
 
             }
         });
